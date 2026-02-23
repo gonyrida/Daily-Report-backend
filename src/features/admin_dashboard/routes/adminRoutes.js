aab9c98b23441211073979d1fbc356a1e25b73f5
Sok-Phal <sokphal019@gmail.com>
2026-02-23 10:32:30 +0700

    feat: in adminController.js add new funcs createUse and updateUser; in adminRoutes.js add route /users for creating user and /user/:id for updating user; in generateSecurePassword.js created to generate random password for user registration; in userModel.js add 3 new fields position, department and orgLevel also adding approver to role field

--- src/features/admin_dashboard/routes/adminRoutes.js
+++ src/features/admin_dashboard/routes/adminRoutes.js
@@ -3,7 +3,9 @@ const express = require("express");
 const { authenticateToken } = require("../../../middleware/authMiddleware");
 const {
   getAllUsers,
-  getRecentUsers
+  getRecentUsers,
+  createUser,
+  updateUser
 } = require("../controllers/adminController");
 
 const router = express.Router();
@@ -11,5 +13,6 @@ const router = express.Router();
 // All admin routes require authentication
 router.get("/all-users", authenticateToken, getAllUsers);
 router.get("/recent-users", authenticateToken, getRecentUsers);
-
+router.post("/users", authenticateToken, createUser);
+router.put("/users/:id", authenticateToken, updateUser);
 module.exports = router;
